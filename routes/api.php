f8294b4fa13a590797313b8189620cd279755222
Shaqlin <shaqlinmondal8905@gmail.com>
2026-02-13 01:56:39 +0530

    ..

--- routes/api.php
+++ routes/api.php
@@ -229,6 +229,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/payments/verify', [PaymentController::class, 'verifyPayment']); // Payment Verification
 
     });
+    
     Route::post('/payments/checkPaymentCorn', [PaymentController::class, 'autoUpdatePendingOrders']); // Payment Verification
     Route::post('/payments/cancelPayment', [PaymentController::class, 'cancelPayment']); // Payment Verification
 });
